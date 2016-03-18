07b3b49b046bb1422a29f4fe58b2caca6e0d8b5c
Mohib Ibrahim <mohibian@Mohibs-MacBook-Air.local>
2016-03-18 12:41:00 -0500

    Added UI Textview
    Added scrolling features.

---
+++ Patience/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Patience
+//
+//  Created by Mohib Ibrahim on 1/23/16.
+//  Xavier University of Louisiana Computer Science
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textView: UIScrollView!
+    //textView.userInteractionEnabled = true
+
+    //@IBOutlet weak var buttonPress: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var myText: UITextView!
+
+}
+
